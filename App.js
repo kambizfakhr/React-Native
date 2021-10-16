7b453d5c68861071420f44053848c9056fbeb348
kambiz fakhr <83270531+kambizfakhr@users.noreply.github.com>
2021-10-16 20:58:26 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+          Hello World
+        </Text>
+      </View>
+      <View style={styles.body}>
+          <Text>
+            sth that not bad
+          </Text>
+        </View>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header:
+  {
+    backgroundColor: 'pink',
+    padding:20,
+  },
+  boldText:
+  {
+    fontWeight: 'bold',
+  },
+  body:
+  {
+    backgroundColor : 'yellow',
+    padding : 20,
+  }
+});
