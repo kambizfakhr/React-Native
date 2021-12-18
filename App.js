31da9fffccdf4b73319faa7b9e3812817201002e
kambiz fakhr <83270531+kambizfakhr@users.noreply.github.com>
2021-12-18 18:12:51 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View , FlatList,TouchableOpacity } from 'react-native';
+import Header from './components/header';
+export default function App() {
+ const [todos , setTodos] = useState([
+   {text: 'buy cofee' , key: '1'},
+   {text: 'create an app' , key: '2'},
+   {text: 'play on the switch' , key: '3'},
+ ])
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* add todo form */}
+        <View style={styles.list}>
+          <FlatList 
+          data={todos}
+          renderItem={({item}) =>(
+            <Text>{item.text}</Text>
+          )}
+          />
+        </View>
+
+      </View>
+    </View>
+    
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
